2a28df476b391afc482f25712e5ccb70c1fed0c1
BrianDX <boris.zaitsev@devexpress.com>
2019-09-12 16:20:34 +0300

    update 18.2

--- Readme.md
+++ Readme.md
@@ -3,12 +3,10 @@
 
 * [Form1.cs](./CS/Dashboard_ConditionalFormatting_Grid/Form1.cs) (VB: [Form1.vb](./VB/Dashboard_ConditionalFormatting_Grid/Form1.vb))
 <!-- default file list end -->
-# How to apply conditional formatting to Grid cells using the Bar Color Ranges condition
+# How to Apply Bar Color Range Conditional Formatting to Grid Cells
 
+The Bar Color Ranges condition - the [FormatConditionColorRangeBar](docs.devexpress.devx/Dashboard/DevExpress.DashboardCommon.FormatConditionColorRangeBar) class - allows you to visualize numeric values using bars whose colors are contained in the specified color set.
 
-<p>The Bar Color Ranges condition (<a href="https://documentation.devexpress.com/#Dashboard/clsDevExpressDashboardCommonFormatConditionColorRangeBartopic">FormatConditionColorRangeBar</a>) allows you to visualize numeric values using bars whose colors are contained in the specified color set.</p>
-<p>This example shows how to display colored bars within Grid cells. Use the <strong>Update Formatting</strong> button to change the number of ranges, specify new range boundaries and customize colors corresponding to existing ranges.</p>
+This example shows how to display colored bars within Grid cells. Click the **Update Formatting** button to change the number of ranges, specify new range boundaries and change colors mapped to ranges.
 
-<br/>
-
-
+![](/images/screenshot.png)
--- VB/Dashboard_ConditionalFormatting_Grid/Form1.vb
+++ VB/Dashboard_ConditionalFormatting_Grid/Form1.vb
@@ -1,42 +1,47 @@
-﻿Imports DevExpress.DashboardCommon
+﻿Imports System
+Imports DevExpress.DashboardCommon
+Imports DevExpress.DashboardWin
 
 Namespace Grid_IconRangeCondition
-    Partial Public Class Form1
-        Inherits DevExpress.XtraEditors.XtraForm
+	Partial Public Class Form1
+		Inherits DevExpress.XtraEditors.XtraForm
 
-        Public Sub New()
-            InitializeComponent()
-            Dim dashboard As New Dashboard()
-            dashboard.LoadFromXml("..\..\Data\Dashboard.xml")
-            dashboardViewer1.Dashboard = dashboard
-            Dim grid As GridDashboardItem = CType(dashboard.Items("gridDashboardItem1"), GridDashboardItem)
-            Dim extendedPrice As GridMeasureColumn = CType(grid.Columns(1), GridMeasureColumn)
+		Public Sub New()
+			InitializeComponent()
+			AddHandler dashboardViewer1.CustomizeDashboardTitle, AddressOf DashboardViewer1_CustomizeDashboardTitle
+			Dim dashboard As New Dashboard()
+			dashboard.LoadFromXml("..\..\Data\Dashboard.xml")
+			dashboardViewer1.Dashboard = dashboard
+			Dim grid As GridDashboardItem = CType(dashboard.Items("gridDashboardItem1"), GridDashboardItem)
+			Dim extendedPrice As GridMeasureColumn = CType(grid.Columns(1), GridMeasureColumn)
 
-            Dim rangeRule As New GridItemFormatRule(extendedPrice)
-            Dim rangeBarCondition As New  _
-                FormatConditionColorRangeBar(FormatConditionRangeSetPredefinedType.ColorsRedGreenBlue)
-            rangeBarCondition.BarOptions.ShowBarOnly = True
-            rangeRule.Condition = rangeBarCondition
+			Dim rangeRule As New GridItemFormatRule(extendedPrice)
+			Dim rangeBarCondition As New FormatConditionColorRangeBar(FormatConditionRangeSetPredefinedType.ColorsRedGreenBlue)
+			rangeBarCondition.BarOptions.ShowBarOnly = True
+			rangeRule.Condition = rangeBarCondition
 
-            grid.FormatRules.AddRange(rangeRule)
-        End Sub
+			grid.FormatRules.AddRange(rangeRule)
+		End Sub
 
-        Private Sub button1_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles button1.Click
-            Dim grid As GridDashboardItem =
-                CType(dashboardViewer1.Dashboard.Items("gridDashboardItem1"), GridDashboardItem)
-            Dim rangeRule As GridItemFormatRule = grid.FormatRules(0)
-            Dim rangeBarCondition As FormatConditionColorRangeBar =
-                CType(rangeRule.Condition, FormatConditionColorRangeBar)
-            Dim range3 As RangeInfo = rangeBarCondition.RangeSet(2)
-            range3.Value = 50
-            range3.StyleSettings = New BarStyleSettings(FormatConditionAppearanceType.GradientGreen)
+		Private Sub DashboardViewer1_CustomizeDashboardTitle(ByVal sender As Object, ByVal e As CustomizeDashboardTitleEventArgs)
+			Dim itemUpdate As New DashboardToolbarItem(Sub(args) UpdateFormatting()) With {.Caption = "Update Formatting"}
+			e.Items.Add(itemUpdate)
+		End Sub
 
-            Dim range4 As New RangeInfo()
-            range4.Value = 75
-            range4.StyleSettings = New BarStyleSettings(FormatConditionAppearanceType.GradientBlue)
-            rangeBarCondition.RangeSet.Add(range4)
+		Private Sub UpdateFormatting()
+			Dim grid As GridDashboardItem = CType(dashboardViewer1.Dashboard.Items("gridDashboardItem1"), GridDashboardItem)
+			Dim rangeRule As GridItemFormatRule = grid.FormatRules(0)
+			Dim rangeBarCondition As FormatConditionColorRangeBar = CType(rangeRule.Condition, FormatConditionColorRangeBar)
+			Dim range3 As RangeInfo = rangeBarCondition.RangeSet(2)
+			range3.Value = 50
+			range3.StyleSettings = New BarStyleSettings(FormatConditionAppearanceType.GradientGreen)
 
-            rangeRule.Condition = rangeBarCondition
-        End Sub
-    End Class
+			Dim range4 As New RangeInfo()
+			range4.Value = 75
+			range4.StyleSettings = New BarStyleSettings(FormatConditionAppearanceType.GradientBlue)
+			rangeBarCondition.RangeSet.Add(range4)
+
+			rangeRule.Condition = rangeBarCondition
+		End Sub
+	End Class
 End Namespace
